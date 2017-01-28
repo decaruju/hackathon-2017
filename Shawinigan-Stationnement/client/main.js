673f6809809402ece7cc0bf30271d77bce1d9339
Polensky <sirois.charles.92@gmail.com>
2017-01-28 16:43:00 -0500

    bounding map box

--- Shawinigan-Stationnement/client/main.js
+++ Shawinigan-Stationnement/client/main.js
@@ -1,19 +1,18 @@
 const MY_ACCESS_TOKEN = 'pk.eyJ1IjoicG9sZW4iLCJhIjoiY2l5aG9mdWkyMDU2MDJ3b2VoYjF4MWN0dSJ9.tVz5AQpyWKIxqX_x-FHRpg';
 const MY_MAP_ID = 'mapbox.satellite';
 
-var bounds = [
-    [-74.04728500751165, 40.68392799015035], // Southwest coordinates
-    [-73.91058699000139, 40.87764500765852]  // Northeast coordinates
-];
 
-Meteor.startup(function (){
-  Mapbox.load();
-})
-
-Deps.autorun(function () {
-  if (Mapbox.loaded()) {
-    L.mapbox.accessToken = MY_ACCESS_TOKEN;
-    var map = new L.mapbox.map('map', MY_MAP_ID);
-    map.center = [100,100];
-  }
+Meteor.startup(function () {
+  mapboxgl.accessToken = 'pk.eyJ1IjoicG9sZW4iLCJhIjoiY2l5aG9mdWkyMDU2MDJ3b2VoYjF4MWN0dSJ9.tVz5AQpyWKIxqX_x-FHRpg';
+  var bounds = [
+    [-72.782743, 46.525046], // Southwest coordinates
+    [-72.714694, 46.564083]  // Northeast coordinates
+  ];
+  var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
+    center: [-72.749588, 46.541601], // starting position
+    maxBounds: bounds,
+    zoom: 14 // starting zoom
+  });
 })
