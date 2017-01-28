3cf0c5f89f1ed3179519816dc119c470530c3913
Polensky <sirois.charles.92@gmail.com>
2017-01-28 16:03:52 -0500

    MAP FORNCTIONEE

--- Shawinigan-Stationnement/client/main.js
+++ Shawinigan-Stationnement/client/main.js
@@ -1,37 +1,19 @@
-import { Template } from 'meteor/templating';
+const MY_ACCESS_TOKEN = 'pk.eyJ1IjoicG9sZW4iLCJhIjoiY2l5aG9mdWkyMDU2MDJ3b2VoYjF4MWN0dSJ9.tVz5AQpyWKIxqX_x-FHRpg';
+const MY_MAP_ID = 'mapbox.satellite';
 
-const MY_ACCESS_TOKEN = pk.eyJ1IjoicG9sZW4iLCJhIjoiY2l5aG9mdWkyMDU2MDJ3b2VoYjF4MWN0dSJ9.tVz5AQpyWKIxqX_x-FHRpg;
-const MY_MAP_ID = lacarte;
+var bounds = [
+    [-74.04728500751165, 40.68392799015035], // Southwest coordinates
+    [-73.91058699000139, 40.87764500765852]  // Northeast coordinates
+];
 
-Mapbox.load({
-  gl: true
-});
-
-// Basic
-Meteor.startup(function(){
-    Mapbox.load({
-        plugins: ['minimap', 'markercluster']
-    });
-});
+Meteor.startup(function (){
+  Mapbox.load();
+})
 
 Deps.autorun(function () {
   if (Mapbox.loaded()) {
     L.mapbox.accessToken = MY_ACCESS_TOKEN;
-    var map = L.mapbox.map('map', MY_MAP_ID);
+    var map = new L.mapbox.map('map', MY_MAP_ID);
+    map.center = [100,100];
   }
-});
-
-
-// Using a template's rendered callback
-Meteor.startup(function(){
-    Mapbox.load();
-});
-
-Template.Map.rendered = function () {
-    this.autorun(function () {
-        if (Mapbox.loaded()) {
-            L.mapbox.accessToken = TOKEN;
-            var map = L.mapbox.map('map', MAP_ID);
-        }
-    });
-};
+})
