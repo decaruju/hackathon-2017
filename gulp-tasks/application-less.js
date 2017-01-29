026e89f84a2bd87f616e30e6212ce060ce48d0f4
Pascal Godin <godin_boy@msn.com>
2017-01-28 20:55:11 -0500

    move public folder for meteor app

--- gulp-tasks/application-less.js
+++ gulp-tasks/application-less.js
@@ -4,6 +4,6 @@ module.exports = function (gulp, plugins, opts) {
           .pipe(plugins.rename(opts.name))
           .pipe(opts.cleanCSS ? plugins.cleanCSS() : plugins.gutil.noop())
           .pipe(plugins.autoprefixer())
-          .pipe(gulp.dest("Shawinigan-Stationnement/public/assets/css/min/"))
+          .pipe(gulp.dest("Shawinigan-Stationnement/client/public/assets/css/min/"))
           .pipe(gulp.dest("public/assets/css/min/"))
 };
